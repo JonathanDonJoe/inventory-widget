51196b58034a39d884c40ecb10d0fe034e2d2a05
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-09 09:58:32 -0400

    Created basic backbone.  Created and added sanity checks to FulterableProductTable, ProductTable, and SearchBar

---
+++ src/ProductTable.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+function ProductTable(props) {
+
+    return(
+        <h1> Product Table</h1>
+    )
+}
+
+export default ProductTable;
---
+++ src/SearchBar.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+function Searchbar(props) {
+
+
+    return(
+        <h1>Search Bar</h1>
+    )
+}
+
+export default Searchbar;
